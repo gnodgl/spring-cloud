f2535c15e93b37022ffb379db3886e2963940d58
神隐` <llgdong@qq.com>
2021-06-11 21:10:42 +0800

    添加gateway

--- spring-cloud-eureka-consumer/src/main/java/xyz/lgdong/springcloudeurekacomsumer/controller/ConsumerController.java
+++ spring-cloud-eureka-consumer/src/main/java/xyz/lgdong/springcloudeurekacomsumer/controller/ConsumerController.java
@@ -2,6 +2,7 @@ package xyz.lgdong.springcloudeurekacomsumer.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import xyz.lgdong.springcloudeurekacomsumer.service.ConsumerService;
 
@@ -11,6 +12,7 @@ import xyz.lgdong.springcloudeurekacomsumer.service.ConsumerService;
  */
 
 @RestController
+@RequestMapping("/consumer")
 public class ConsumerController {
 
     @Autowired
